dd1bef90ee9798bcc2afec1adc76820bceacf4b4
Leonardo <leo.luz14@hotmail.com>
2019-11-18 16:36:52 -0300

    Style/instagram start

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import "./Header.css";
+import "./header.css";
 
 import logoInsta from "../assets/instagram.svg";
 import logoLinkedin from "../assets/linkedin.svg";
 import logoFacebook from "../assets/facebook.svg";
 import logoGithub from "../assets/github.svg";
 import logoTwitter from "../assets/twitter.svg";
+import logoTwitch from "../assets/twitch.svg";
 
 export default function Header() {
   return (
-    <header id="-main-header">
+    <header id="main-header">
       <div className="header-content">
-        <img src={logoInsta} alt="instagram" />
-        <img src={logoLinkedin} alt="linkedin" />
-        <img src={logoFacebook} alt="facebook" />
-        <img src={logoGithub} alt="github" />
-        <img src={logoTwitter} alt="twitter" />
+        <h1>Meu Site Pessoal</h1>
+        <Link to="https://www.twitch.tv/leouluz">
+          <img className=" imagenslogo" src={logoLinkedin} alt="Linkedin" />
+        </Link>
+        <Link to="https://www.twitch.tv/leouluz">
+          <img className=" imagenslogo" src={logoFacebook} alt="Facebook" />
+        </Link>
+        <Link to="https://www.twitch.tv/leouluz">
+          <img className=" imagenslogo" src={logoTwitter} alt="Twitter" />
+        </Link>
+        <Link to="https://www.twitch.tv/leouluz">
+          <img className=" imagenslogo" src={logoGithub} alt="GitHub" />
+        </Link>
+        <Link to="https://www.twitch.tv/leouluz">
+          <img className=" imagenslogo" src={logoTwitch} alt="Twitch" />
+        </Link>
+        <Link to="https://www.twitch.tv/leouluz">
+          <img className=" imagenslogo" src={logoInsta} alt="instagram" />
+        </Link>
       </div>
     </header>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class Home extends Component{
-  render(){
-    return(
-      <h1>Home</h1>
+import "./Home.css";
+
+import Foto from "../assets/fotos/fotohack.jfif";
+
+class Home extends Component {
+  render() {
+    return (
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Evento: </span>
+              <span className="place">Hackathon - SpaceApps - Nasa</span>
+            </div>
+
+            <img src={Foto} alt="" />
+
+            <footer>
+              <div className="actions">
+                <p>like</p>
+                <p>coment</p>
+                <p>send</p>
+              </div>
+              <strong>curtidas</strong>
+              <p>
+                texto dhsuaidhusaidhusa dhusaihd dshauidhas
+                <span>#hashtag</span>
+              </p>
+            </footer>
+          </header>
+        </article>
+      </section>
     );
   }
 }
